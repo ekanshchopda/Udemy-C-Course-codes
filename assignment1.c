6b918227143e90db656a23ff3f547795803162b5
ekanshchopda <70812692+ekanshchopda@users.noreply.github.com>
2026-03-30 11:45:31 +0000

    arith methods

---
+++ assignment1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two numbers:");
+    scanf("%d %d",&a,&b);
+    printf("Addition: %d\n",a+b);
+    printf("Subtraction: %d\n",a-b);
+    printf("Multpliaction: %d\n",a*b);
+    printf("Division:%d\n",a/b);
+    return 0;
+}
